0e962bfe8995b157e31057741089a949b01aa4c3
DharmikGohil013 <dharmikgohil395003@gmail.com>
2024-09-20 16:18:47 +0530

    add

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,22 +1,56 @@
-// Get the elements
-const menuToggle = document.getElementById('menu-toggle');
-const dropdownMenu = document.getElementById('dropdown-menu');
+// Navbar 
+const menu = document.querySelector('.menu')
+const navbar = document.querySelector('.navbar')
 
-// Toggle dropdown menu visibility on click
-menuToggle.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('active'); // Toggle 'active' class
-});
-// Show the notification after 5 seconds
-setTimeout(() => {
-    const notification = document.getElementById('notification');
-    notification.style.display = 'block'; // Show the notification
-}, 2000);
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('change')
+    menu.classList.toggle('change')
+})
+// End of Navbar 
 
-// Close the notification when the close button is clicked
-document.addEventListener('DOMContentLoaded', () => {
-    const closeButton = document.getElementById('close-button');
-    closeButton.addEventListener('click', () => {
-        const notification = document.getElementById('notification');
-        notification.style.display = 'none'; // Hide the notification
-    });
-});
+// Section 2 Video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+    if(video.paused) {
+        video.play()
+        btn.className = 'far fa-pause-circle'
+        video.style.opacity = '.7'
+    } else {
+        video.pause()
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '.3'
+    }
+}
+
+btn.addEventListener('click', () => {
+    playPause()
+})
+
+video.addEventListener('timeupdate', () => {
+    const barWidth = video.currentTime / video.duration
+    bar.style.width = `${barWidth * 100}%`
+    if(video.ended) {
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '.3'
+    }
+})
+// End of Section 2 Video
+
+// Section 3 Pricing Cards
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 70,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
+// End of Section 3 Pricing Cards
