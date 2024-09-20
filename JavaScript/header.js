8c4a80971954866a3f3e3a51f954bab956aac1d3
DharmikGohil013 <dharmikgohil395003@gmail.com>
2024-09-20 09:19:51 +0530

    More Css changes

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -6,3 +6,17 @@ const dropdownMenu = document.getElementById('dropdown-menu');
 menuToggle.addEventListener('click', () => {
     dropdownMenu.classList.toggle('active'); // Toggle 'active' class
 });
+// Show the notification after 5 seconds
+setTimeout(() => {
+    const notification = document.getElementById('notification');
+    notification.style.display = 'block'; // Show the notification
+}, 2000);
+
+// Close the notification when the close button is clicked
+document.addEventListener('DOMContentLoaded', () => {
+    const closeButton = document.getElementById('close-button');
+    closeButton.addEventListener('click', () => {
+        const notification = document.getElementById('notification');
+        notification.style.display = 'none'; // Hide the notification
+    });
+});
